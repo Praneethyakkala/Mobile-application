017bc4dd55566cf6f50f8a110acb9a7ffe7ced38
Praneethyakkala <yakkala.sripraneeth.20cse@bmu.edu.in>
2022-09-25 22:52:05 +0530

    Added view profile page!

---
+++ app/src/main/java/com/bmlmunjal/mad/viewPageActivity.java
@@ -0,0 +1,14 @@
+package com.bmlmunjal.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class viewPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_page);
+    }
+}
